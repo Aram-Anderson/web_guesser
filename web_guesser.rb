78bfa797636a0d66b115ae1930cd721855f284c9
Aram Anderson <aram.j.anderson@gmail.com>
2017-08-07 08:26:35 -0600

    Rewrite and refactor

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,54 @@ require 'sinatra/reloader'
 num = rand(1..100)
 
 get '/' do
-  erb :index, :locals => {:num => num}
+	guess = params["guess"]
+	message, color = check_guess(guess, num)
+	erb :index, :locals => { :message => message, :color => color }
+end
+
+def check_guess(guess, num)
+	if guess_does_not_exist(guess)
+		message = "Guess a number between 0 and 100."
+		color   = "white"
+	elsif guess_is_correct?(guess, num)
+		message = "You got it right! The secret number was #{num}."
+		color   = "green"
+	elsif guess_is_high_but_close(guess, num)
+		message =  "Too high, but close! Try again!"
+		color   = "yellow"
+	elsif guess_is_low_but_close(guess, num)
+		message = "Too low, but close! Try again!"
+		color   = "yellow"
+	elsif guess_is_way_too_high(guess, num)
+		message = "Way too high. Try again."
+		color   = "red"
+	elsif guess_is_way_too_low(guess, num)
+		message = "Way too low. Try again."
+		color   = "red"
+	end
+	[message, color]
+end
+
+def guess_does_not_exist(guess)
+	!((1..100).cover? guess.to_i)
+end
+
+def guess_is_correct?(guess, num)
+	guess.to_i == num
+end
+
+def guess_is_high_but_close(guess, num)
+	guess.to_i > num && guess.to_i <= num  + 5
+end
+
+def guess_is_low_but_close(guess, num)
+	guess.to_i < num && guess.to_i >= num - 5
+end
+
+def guess_is_way_too_high(guess, num)
+	guess.to_i > num + 5
+end
+
+def guess_is_way_too_low(guess, num)
+	guess.to_i < num - 5
 end
